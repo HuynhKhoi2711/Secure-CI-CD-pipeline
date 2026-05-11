1a0ac7d952eeaeaf71b38ed099ea6e5401b7cb8a
HuynhKhoi2711 <huynhanhkhoi9a7@gmail.com>
2026-05-11 10:04:48 +0700

    ci: add kubernetes provider configuration

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,9 +4,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
+}
+
+provider "kubernetes" {
+  config_context_cluster = "minikube"
+  config_path = "~/.kube/config"
 }
 
 provider "aws" {
   region = "us-east-1"
+
 }
+
