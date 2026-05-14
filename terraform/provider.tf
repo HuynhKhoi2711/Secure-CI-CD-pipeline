078cbd9abe82c30744721da3d86687d93bf55094
HuynhKhoi2711 <huynhanhkhoi9a7@gmail.com>
2026-05-14 17:20:00 +0700

    ci: add MongoDB and NodeGoat deployment and service configurations

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -22,5 +22,5 @@ provider "kubernetes" {
   cluster_ca_certificate = kind_cluster.nodegoat.cluster_ca_certificate
   client_certificate     = kind_cluster.nodegoat.client_certificate
   client_key             = kind_cluster.nodegoat.client_key
-  config_path = "~/.kube/config"
+  # config_path = "~/.kube/config"
 }
