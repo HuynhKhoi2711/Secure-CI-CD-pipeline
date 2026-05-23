411e9d3f64f6bd459e9e1345c2a61e9eb69a4666
HuynhKhoi2711 <huynhanhkhoi9a7@gmail.com>
2026-05-23 18:41:53 +0700

    testing_s3_ec2_pipeline

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,19 +13,3 @@ variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
 }
-
-
-variable "S3_BUCKET" {
-  description = "The name of the S3 bucket for Terraform state storage"
-  type        = string
-}
-
-variable "S3_REGION" {
-  description = "The AWS region where the S3 bucket is located"
-  type        = string
-}
-
-variable "DYNAMODB_TABLE" {
-  description = "The name of the DynamoDB table for Terraform state locking"
-  type        = string
-}
