36352aeeb1da985e648ea65c605fc885b6c45b77
HuynhKhoi2711 <huynhanhkhoi9a7@gmail.com>
2026-05-23 18:37:11 +0700

    testing_s3_ec2_pipeline

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.50"
+      version = ">= 6.28.0"
     }
     random = {
       source  = "hashicorp/random"
